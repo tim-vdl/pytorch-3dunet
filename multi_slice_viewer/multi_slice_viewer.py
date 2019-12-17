2b58a688430193e323391702570906a23907da41
Tim Van De Looverbosch <u0117721@kuleuven.be>
2019-12-17 18:16:41 +0100

    add tool to visualize slices

---
+++ multi_slice_viewer/multi_slice_viewer.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+
+def multi_slice_viewer(volume):
+    fig, ax = plt.subplots()
+    ax.volume = volume
+    ax.index = volume.shape[0] // 2
+    ax.imshow(volume[ax.index])
+    fig.canvas.mpl_connect('key_press_event', process_key)
+    plt.show()
+
+def process_key(event):
+    fig = event.canvas.figure
+    ax = fig.axes[0]
+    if event.key == 'j':
+        previous_slice(ax)
+    elif event.key == 'k':
+        next_slice(ax)
+    fig.canvas.draw()
+
+def previous_slice(ax):
+    """Go to the previous slice."""
+    volume = ax.volume
+    ax.index = (ax.index - 1) % volume.shape[0]  # wrap around using %
+    ax.images[0].set_array(volume[ax.index])
+
+def next_slice(ax):
+    """Go to the next slice."""
+    volume = ax.volume
+    ax.index = (ax.index + 1) % volume.shape[0]
+    ax.images[0].set_array(volume[ax.index])
