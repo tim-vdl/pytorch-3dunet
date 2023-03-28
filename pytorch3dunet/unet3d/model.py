e05d0a571430986107597fb19a40b355331e3e70
Tim Van De Looverbosch <tvdlooverbosch@sto3.visielab.uantwerpen.be>
2023-03-28 11:41:56 +0200

    Use relative imports

--- pytorch3dunet/unet3d/model.py
+++ pytorch3dunet/unet3d/model.py
@@ -1,8 +1,8 @@
 import torch.nn as nn
 
-from pytorch3dunet.unet3d.buildingblocks import DoubleConv, ExtResNetBlock, create_encoders, \
+from .buildingblocks import DoubleConv, ExtResNetBlock, create_encoders, \
     create_decoders
-from pytorch3dunet.unet3d.utils import number_of_features_per_level, get_class
+from .utils import number_of_features_per_level, get_class
 
 
 class Abstract3DUNet(nn.Module):
@@ -89,7 +89,8 @@ class Abstract3DUNet(nn.Module):
         x = self.final_conv(x)
 
         # apply final_activation (i.e. Sigmoid or Softmax) only during prediction. During training the network outputs logits
-        if not self.training and self.final_activation is not None:
+        #if not self.training and self.final_activation is not None:
+        if self.final_activation is not None:
             x = self.final_activation(x)
 
         return x
